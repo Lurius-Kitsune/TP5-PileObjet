ec49d15538df2c6ca5b77384ac7306006db9a54e
Lurius-Kitsune <lucasfox@outlook.fr>
2023-01-30 16:33:10 +0100

    [New] Contructeur + définition des variable

--- (TP5)PileObjet/Pile.cs
+++ (TP5)PileObjet/Pile.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace _TP5_PileObjet
 {
-    internal class Pile
-    {
+    /// <summary>
+    /// Pile contenant pouvant contenir des entiers.
+    /// On ajoute après le dernier élément ajouté
+    /// On retire toujours le dernier élément ajouté
+    /// </summary>
+    class Pile
+    {   
+        /// <summary>
+        /// Nombre maximum d'éléments de la Pile
+        /// </summary>
+        private int nbMaxElt;
+
+        /// <summary>
+        /// Liste contenant les éléments de la pile
+        /// </summary>
+        private List<int> elements;
+
+        /// <summary>
+        /// Constructeur de la classe Pile
+        /// </summary>
+        /// <param name="nbMaxElt">Taille maximum de la pile</param>
+        public Pile(int nbMaxElt)
+        {
+            this.nbMaxElt = nbMaxElt;
+            this.elements = new List<int>();
+        }
     }
 }
