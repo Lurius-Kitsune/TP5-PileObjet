43060f2f13262c3c06d523cc279ef8976d2f801c
Lurius-Kitsune <lucasfox@outlook.fr>
2023-02-01 08:53:50 +0100

    [New] Ajout de nouvelle methode Surcharge SaisirNb

--- (TP5)PileObjet/Utilitaire.cs
+++ (TP5)PileObjet/Utilitaire.cs
@@ -8,5 +8,33 @@ namespace Utilitaires
 {
     public abstract class Utilitaire
     {
+        public int SaisirNb()
+        {
+            Console.WriteLine("Veuillez saisir un nombre entier : ");
+            int nb = Convert.ToInt32(Console.ReadLine());
+            return nb;
+        }
+
+        public int SaisirNb(int pMin)
+        {
+            int nb = 0;
+            while (nb >= pMin)
+            {
+                Console.WriteLine($"Veuillez saisir un nombre entier de minimum {pMin}: ");
+                nb = Convert.ToInt32(Console.ReadLine());
+            }
+            return nb;
+        }
+
+        public int SaisirNb(int pMin, int pMax)
+        {
+            int nb = 0;
+            while (nb >= pMin && nb <= pMax)
+            {
+                Console.WriteLine($"Veuillez saisir un nombre entier compris entre {pMin} et {pMax}: ");
+                nb = Convert.ToInt32(Console.ReadLine());
+            }
+            return nb;
+        }
     }
 }
