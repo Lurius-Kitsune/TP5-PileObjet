4c8eb8bd9ca6dd4e6d6b2a15200b61af7b789cd4
Lurius-Kitsune <lucasfox@outlook.fr>
2023-02-02 18:50:57 +0100

    [New] Ajout de la methode RécupLoremIpsum

--- (TP5)PileObjet/Utilitaires/UtilitaireAPI.cs
+++ (TP5)PileObjet/Utilitaires/UtilitaireAPI.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Utilitaires
 {
-    internal class UtilitaireAPI
+    public static class UtilitaireAPI
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nbParagraphes"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        static String RecupereLoremIpsum(int nbParagraphes)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/text"));
+            String url = $"https://loripsum.net/api/{nbParagraphes}/short/plaintext";
+            var response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                return responseBody;
+            }
+            else
+            {
+                throw new Exception($"Erreur API : {response.StatusCode} {response.ReasonPhrase}");
+            }
+        }
     }
 }
